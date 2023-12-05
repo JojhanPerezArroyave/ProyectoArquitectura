dc40a441dd3c0889d266a3379c8f41d5eda7deb8
Jojhan Perez Arroyave <jojhan.arroyavea@opitech.com.co>
2023-12-05 08:30:31 -0500

    Segunda version de la interfaz

--- src/app/shared/services/task-execute.service.ts
+++ src/app/shared/services/task-execute.service.ts
@@ -3,15 +3,14 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-
 export class TaskExecuteService {
 
   timeForExecution(task: any) {
-    return new Promise((resolve, reject) => {
+    return new Promise<void>((resolve, reject) => { 
       setTimeout(() => {
         task();
-        resolve;
+        resolve(); 
       }, 800);
-    })
-  };
+    });
+  }
 }
